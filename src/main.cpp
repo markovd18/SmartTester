7b5bb46addbd181805df034440829afdd1c20e2a
David Markov <markovda@marbes.cz>
2020-04-20 10:06:45 +0200

    Jako parametr je nyní zadán GUID filtru, který chceme testovat.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "TestFilter.h"
 
 #ifdef _WIN32
- wchar_t* LIB_DIR = L"../../smartcgms/windows_64/filters/";
+const wchar_t* LIB_DIR = L"../../smartcgms/windows_64/filters/";
 #elif __APPLE__
 const wchar_t* LIB_DIR = L"../../smartcgms/macos_64/filters/";
 #else
@@ -20,20 +20,81 @@ const wchar_t* LIB_DIR = L"../../smartcgms/debian_64/filters/";
 */
 void print_help(){
     std::wcerr << "Execute with two parameters <test_type> <tested_subject>\n"
-                  "<test_type> ... '-u' = filter unit tests, '-r' = scenario regression tests\n"
-                  "<tested_subject> ... <filter_path> - filter to test with unit tests, "
+                  "<test_type> ... '-u' = filter unit tests / '-r' = scenario regression tests\n"
+                  "<tested_subject> ... <filter_guid> - GUID of filter to test with unit tests / "
                   "<config_path> - path to filter chain config file\n"
-                  "a) -u <filter_path>\n"
+                  "a) -u <filter_guid>\n"
                   "b) -r <config_path>\n";
+}
+
+/**
+    Parses guid in string from command-line into hexadecimal numbers and creates GUID structure.
+*/
+GUID parse_guid(std::string guid_string) {
+    GUID guid{};
+
+    std::string delimeter = "-";
+    std::string token;
+    std::string sub_token;
+    size_t position = 0;
+    int counter = 0;
+
+    try {
+        while ((position = guid_string.find(delimeter)) != std::string::npos) {
+            token = guid_string.substr(0, position);
+
+            switch (counter) {
+            case 0:
+                guid.Data1 = std::stoul(token, nullptr, 16);
+                break;
+            case 1:
+                guid.Data2 = std::stoi(token, nullptr, 16);
+                break;
+            case 2:
+                guid.Data3 = std::stoi(token, nullptr, 16);
+                break;
+            case 3:
+                for (int i = 0; i < 2; i++) {
+                    sub_token = token.substr(0, 2 * sizeof(char));
+                    guid.Data4[i] = std::stoi(sub_token, nullptr, 16);
+                    token.erase(0, 2 * sizeof(char));
+                }
+                break;
+            default:
+                std::wcerr << L"Invalid format of GUID inserted!\n";
+                exit(2);
+            }
+
+            guid_string.erase(0, position + delimeter.length());
+            counter++;
+        }
+
+        for (int i = 2; i < 8; i++) {
+            token = guid_string.substr(0, 2 * sizeof(char));
+            guid.Data4[i] = std::stoi(token, nullptr, 16);
+            guid_string.erase(0, 2 * sizeof(char));
+        }
+    } catch (std::runtime_error) {
+            std::wcerr << L"Invalid format of GUID inserted!\n";
+            exit(2);
+    }
+
+    return guid;
 }
 
 /**
     Loads library of given filter and starts unit tests.
 */
-int execute_unit_testing(const char *filter_name){
+int execute_unit_testing(std::string guid_string){
     CDynamic_Library::Set_Library_Base(LIB_DIR);
-
     CDynamic_Library library;
+    GUID guid;
+
+    if (!guid_string.empty())
+    {
+        guid = parse_guid(guid_string);
+    }
+    // id log filtru {C0E942B9-3928-4B81-9B43-A347668200BA}
 
     if (!library.Load(L"log.dll")) {
         std::cout << "Couldn't load library!";
@@ -43,11 +104,8 @@ int execute_unit_testing(const char *filter_name){
     auto creator = library.Resolve<scgms::TCreate_Filter>("do_create_filter");
 
     scgms::IFilter *created_filter = nullptr;
-
-    // id log filtru
-    GUID guid = {0xc0e942b9, 0x3928, 0x4B81, { 0x9b, 0x43, 0xa3, 0x47, 0x66, 0x82, 0x00, 0xba}};
-
     TestFilter filter = TestFilter();
+
     auto result = creator(&guid, &filter, &created_filter);
     if (result == S_OK){
         std::cout << "Filter created succesfully.";
@@ -69,19 +127,21 @@ int execute_regression_testing(const char* config_path){
 */
 int MainCalling main(int argc, char * argv[])
 {
-    if (argc < 3){
+    if (argc < 2){
         std::wcerr << L"Wrong parameter count!\n";
         print_help();
         return 1;
     }
 
     if (argv[1][0] == '-'){
+        std::string parameter = "";
         switch (argv[1][1]){
-            case 'u':
-                //TODO unit testing
-                return execute_unit_testing(argv[2]);
-            case 'r':
-                //TODO regression testing
+            case 'u':   // unit testing
+                if (argv[2] != NULL) {
+                    parameter = argv[2];
+                }
+                return execute_unit_testing(parameter);
+            case 'r':   // regression testing
                 return execute_regression_testing(argv[2]);
             default:
                 std::wcerr << L"Unknown type of testing requested!\n";
